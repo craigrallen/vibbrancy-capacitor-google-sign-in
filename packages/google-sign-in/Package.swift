173af3a7ae47dfc74a77a7b67e841c2b2a08030a
Craig Allen <craig@interconnected.au>
2026-03-25 08:34:20 +0100

    chore: commit built dist files for npm install from GitHub

--- packages/google-sign-in/Package.swift
+++ packages/google-sign-in/Package.swift
@@ -2,11 +2,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "CapawesomeCapacitorGoogleSignIn",
+    name: "VibbrancyCapacitorGoogleSignIn",
     platforms: [.iOS(.v15)],
     products: [
         .library(
-            name: "CapawesomeCapacitorGoogleSignIn",
+            name: "VibbrancyCapacitorGoogleSignIn",
             targets: ["GoogleSignInPlugin"])
     ],
     dependencies: [
